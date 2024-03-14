ca894649477151e53667224b82ae327e28ce0811
ajeglon <61421392+ajeglon@users.noreply.github.com>
2024-03-14 15:42:04 +0000

    updated tables

--- matex_app/models.py
+++ matex_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class CertificateHolder(models.Model):
-    nhs_number = models.IntegerField(primary_key=True)
+    nhs_number = models.IntegerField()
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
